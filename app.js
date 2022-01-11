abaa96a7e16585611049dec78da71b21cbf1e2e8
1988db <elozrana@gmail.com>
2022-01-11 18:52:09 +0100

    game ready

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const grid = document.querySelector('.grid');
     var cardsChosen = [];
     var cardsChosenId = [];
+    var cardsWon = [];
+    var cardsWonId = [];
+    const resultDisplay = document.querySelector('#result');
     const cardArray = [
         {
             name: 'canyon',
@@ -52,13 +55,16 @@ document.addEventListener('DOMContentLoaded', ()=> {
             img: 'img/trifox.jpg'
         }
     ];
+
+    //sort cardArray randomly
+    cardArray.sort( ()=> 0.5 - Math.random())
     
     function createBoard() {
         for (let i=0; i < cardArray.length; i++) {
             let card = document.createElement('div');
             card.style.width = '100px';
             card.style.height = '100px';
-            card.style.backgroundImage = "url('img/blank.jpg')";
+            card.style.backgroundImage = "url('img/card.jpg')";
             card.style.backgroundSize = 'cover';
             card.dataset.id = i;
             card.addEventListener('click', flipCard);
@@ -72,12 +78,41 @@ document.addEventListener('DOMContentLoaded', ()=> {
         cardsChosen.push(cardArray[cardId].name);
         cardsChosenId.push(cardId);
         this.style.backgroundImage = "url('" + cardArray[cardId].img + "')";
-        if (cardsChosen === 2) {
+        if (cardsChosen.length === 2) {
+            var cards = document.querySelectorAll('.grid div');
+            cards.forEach(card => card.removeEventListener('click', flipCard));
             setTimeout(checkForMatch, 500);
         }
     }
 
     //check for match
+    function checkForMatch() {
+        var cards = document.querySelectorAll('.grid div');
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match!')
+            cards[optionOneId].style.backgroundImage = "url('img/blank.jpg')";
+            cards[optionTwoId].style.backgroundImage = "url('img/blank.jpg')";
+            cardsWon.push(cardsChosen);
+            cardsWonId.push(cardsChosenId[0]);
+            cardsWonId.push(cardsChosenId[1]);
+            console.log(cardsWonId)
+        } else {
+            cards[optionOneId].style.backgroundImage = "url('img/card.jpg')";
+            cards[optionTwoId].style.backgroundImage = "url('img/card.jpg')"
+            alert('Sorry, try again!');
+        }
+        cardsChosen = [];
+        cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+        cards.forEach(card => card.addEventListener('click', flipCard));
+        cardsWonId.forEach(cardWonId => cards[cardWonId].removeEventListener('click', flipCard));
+        if(cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'Congratulations. You won!'
+        }
+        
+    }
 
     createBoard();
     
